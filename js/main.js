c279dee75ec552d188de37745aef4b5d2e0e784c
wniebelski <wniebelski@gmail.com>
2022-09-26 09:39:19 +0200

    rozwiązanie

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let edycja = document.getElementById('opis');
+let guzik = document.querySelector('button');
+guzik.onclick = function () {
+    edycja.textContent = 'Bardzo dobry dzień dla wszystkich programistów.';
+};
